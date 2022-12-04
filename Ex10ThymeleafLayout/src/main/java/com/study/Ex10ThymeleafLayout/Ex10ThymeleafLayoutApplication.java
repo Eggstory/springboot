6b07bb49d9ad299d9d9a3472b45913fd0be8e41a
khm9963 <khm9963@naver.com>
2022-12-04 18:12:20 +0900

    테스트겸

---
+++ Ex10ThymeleafLayout/src/main/java/com/study/Ex10ThymeleafLayout/Ex10ThymeleafLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.study.Ex10ThymeleafLayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex10ThymeleafLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex10ThymeleafLayoutApplication.class, args);
+	}
+
+}
---
+++ Ex10ThymeleafLayout/src/test/java/com/study/Ex10ThymeleafLayout/Ex10ThymeleafLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex10ThymeleafLayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex10ThymeleafLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
